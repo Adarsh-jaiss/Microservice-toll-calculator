c31690465195d615eabae10ed29e329bdd5ade72
adarsh-jaiss <its.adarshjaiss@gmail.com>
2024-03-22 22:10:51 +0530

    implemeted invoice calculator

--- aggregator/main.go
+++ aggregator/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/adarsh-jaiss/microservice-toll-calculator/types"
 )
@@ -26,7 +27,44 @@ func main() {
 func makeHTTPTransport(listenAddr string, svc Aggregator) {
 	fmt.Printf("HTTP Transport running at port %s...\n", listenAddr)
 	http.HandleFunc("/aggregate", HandleAggregate(svc))
+	http.HandleFunc("/invoice", HandleGetInvoice(svc))
 	log.Fatal(http.ListenAndServe(listenAddr, nil))
+}
+
+func HandleGetInvoice(svc Aggregator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// fmt.Println(r.URL.Query())
+		values, ok := r.URL.Query()["obu"]
+		if !ok {
+			WriteJSON(w, http.StatusBadRequest, map[string]string{"error": "missing OBU ID"})
+			return
+		}
+		// obuId := r.URL.Query()["obu"][0]
+		fmt.Println(values[0])
+		obuId, err := strconv.Atoi(values[0])
+		if err != nil {
+			WriteJSON(w, http.StatusBadRequest, map[string]string{"error": "inavlid OBU ID "})
+		}
+		fmt.Println(obuId)
+
+		invoice, err := svc.CalculateInvoice(obuId)
+		if err != nil {
+			WriteJSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
+			return
+		}
+
+		WriteJSON(w,http.StatusOK,invoice)
+
+
+		// distance, err := svc.DistanceSum(obuId)
+		// if err != nil {
+		// 	WriteJSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		// }
+
+		// WriteJSON(w, http.StatusOK, map[string]any{"distance"})
+		// w.Write([]byte("need to return the invoice for the OBU ID"))
+	}
 }
 
 func HandleAggregate(svc Aggregator) http.HandlerFunc {
--- types/types.go
+++ types/types.go
@@ -1,7 +1,5 @@
 package types
 
-import ()
-
 type OBUData struct {
 	OBUID     int     `json:"obuID"`
 	Latitiude float64 `json:"latitiude"`
@@ -12,4 +10,10 @@ type Distance struct {
 	Value float64 `json:"value"`
 	OBUID int     `json:"obuID"`
 	Unix  int64   `json:"unix"`
+}
+
+type Invoice struct {
+	OBUID         int     `json:"obuID"`
+	TotalDistance float64 `json:"totalDistance"`
+	TotalAmount   float64 `json:"totalAmount"`
 }
