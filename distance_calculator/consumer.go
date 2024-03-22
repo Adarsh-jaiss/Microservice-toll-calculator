7419d169107977e3af062f18c14b8d1242faeb20
adarsh-jaiss <its.adarshjaiss@gmail.com>
2024-03-22 13:59:31 +0530

    added more elements into kakfka consumer

--- distance_calculator/consumer.go
+++ distance_calculator/consumer.go
@@ -4,44 +4,48 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"time"
+
 	"github.com/adarsh-jaiss/microservice-toll-calculator/types"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/sirupsen/logrus"
 )
 
-type KafkaConsumer struct{
-	consumer *kafka.Consumer
-	IsRunning bool
+// This can also be called Kafka Transport
+type KafkaConsumer struct {
+	consumer    *kafka.Consumer
+	IsRunning   bool
 	CalcService CalculatorServicer
 }
 
-func NewkafkaConsumer(topic string, svc CalculatorServicer) (*KafkaConsumer,error) {
+func NewkafkaConsumer(topic string, svc CalculatorServicer) (*KafkaConsumer, error) {
 
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-        "bootstrap.servers": "localhost",
-        "group.id":          "myGroup",
-        "auto.offset.reset": "earliest",
-    })
-    if err != nil {
-        return nil, fmt.Errorf("failed to create Kafka consumer: %s", err)
-    }
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Kafka consumer: %s", err)
+	}
 
-    err = c.SubscribeTopics([]string{topic}, nil)
-    if err != nil {
-        return nil, fmt.Errorf("failed to subscribe to topic %s: %s", topic, err)
-    }
+	err = c.SubscribeTopics([]string{topic}, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to subscribe to topic %s: %s", topic, err)
+	}
 
-    fmt.Printf("Kafka consumer subscribed to topic: %s\n", topic)
+	fmt.Printf("Kafka consumer subscribed to topic: %s\n", topic)
 
 	// c.Close()
 
 	return &KafkaConsumer{
-		consumer: c,
-		CalcService: svc ,
-	},nil 
+		consumer:    c,
+		CalcService: svc,
+		IsRunning:   true,
+	}, nil
 }
 
-func(c *KafkaConsumer) Start() {
+func (c *KafkaConsumer) Start() {
 	logrus.Info("Kafka transport started")
 	c.IsRunning = true
 	c.ReadMessageLoop()
@@ -49,11 +53,13 @@ func(c *KafkaConsumer) Start() {
 
 func (c *KafkaConsumer) ReadMessageLoop() {
 	for c.IsRunning {
-		msg, err := c.consumer.ReadMessage(-1)
+		msg, err := c.consumer.ReadMessage(time.Minute)
 		if err != nil {
-			logrus.Errorf("kafka consume error %s", err)
+			logrus.Errorf("kafka consume error :: %s", err)
+			// log.Fatal(err)
 			continue
 		}
+		fmt.Println(msg)
 		var data types.OBUData
 		if err := json.Unmarshal(msg.Value, &data); err != nil {
 			logrus.Errorf("JSON serialization error: %s", err)
@@ -64,10 +70,6 @@ func (c *KafkaConsumer) ReadMessageLoop() {
 			logrus.Errorf("calculation error: %s", err)
 			continue
 		}
-		fmt.Printf("distance : %.2f",distance)
+		fmt.Printf("distance : %.2f", distance)
 	}
 }
-
-
-
-	
